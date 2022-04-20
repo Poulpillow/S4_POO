ec034e050f59004ee62a610fbc7a6fe0e3f78f6d
Clémence <clemvoegele@gmail.com>
2022-04-20 15:52:23 +0200

    [noughts_and_crosses] Add a grid

---
+++ src/noughts_and_crosses.cpp
@@ -0,0 +1,27 @@
+#include "noughts_and_crosses.hpp"
+
+void draw_board(p6::Context& context)
+{
+    for (float i = -0.7f; i < 0.8f; i += 0.7f) {
+        for (float j = -0.7f; j < 0.8f; j += 0.7f) {
+            context.fill = {i / 3.0f + 1 / 3.0f, j / 3.0f + 1 / 3.0f, 0};
+            context.square(p6::Center{i, j},
+                           p6::Radius{0.3f});
+        }
+    }
+}
+
+void play_noughts_and_crosses()
+{
+    auto ctx = p6::Context{{1280, 720, "Noughts and Crosses"}};
+    ctx.background({0.5f, 0.3f, 0.8f});
+    draw_board(ctx);
+
+    // ctx.update = [&]() {
+    //     ctx.background({0.5f, 0.3f, 0.8f});
+    //     ctx.square(p6::Center{ctx.mouse()},
+    //                p6::Radius{0.3f});
+    // };
+
+    ctx.start();
+}
---
+++ src/noughts_and_crosses.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <p6/p6.h>
+
+void draw_board(p6::Context& context);
+void play_noughts_and_crosses();
