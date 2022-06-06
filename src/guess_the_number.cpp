f3c450411656343a17d1c6a07e6ae3ad30c3de21
Clémence <clemvoegele@gmail.com>
2022-06-06 15:25:37 +0200

    [guess_the_number] Fix game design

--- src/guess_the_number.cpp
+++ src/guess_the_number.cpp
@@ -7,20 +7,20 @@ void play_guess_the_number()
     const int unknown_number = random_object(1, 100);
     int       round          = 1;
 
-    std::cout << "Welcome to Guess the number !\nPick a number between 1 and 100\n";
+    std::cout << "\nWelcome to Guess the number !\nPick a number between 1 and 100\n";
     int picked_number;
     std::cin >> picked_number;
 
     while (picked_number != unknown_number) {
         if (picked_number > unknown_number) {
-            std::cout << "Smaller\n";
+            std::cout << "Smaller\n\n/----------------------------------------------------/\n";
         }
         else {
-            std::cout << "Greater\n";
+            std::cout << "Greater\n\n/----------------------------------------------------/\n";
         }
-        std::cout << "Try again\n";
+        std::cout << "\nTry again\n";
         std::cin >> picked_number;
         round++;
     }
-    std::cout << "Congrats, you won in " << round << " rounds !\n\n";
+    std::cout << "\nCongrats, you won in " << round << " rounds !\n\n";
 }
