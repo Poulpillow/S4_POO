dbfbfe9cf2708097c70b37383b01602132ff1815
Clémence <clemvoegele@gmail.com>
2022-06-06 15:35:00 +0200

    [guess_the_number] Add comments + Fix game design

--- src/guess_the_number.cpp
+++ src/guess_the_number.cpp
@@ -4,23 +4,25 @@
 
 void play_guess_the_number()
 {
-    const int unknown_number = random_object(1, 100);
+    const int unknown_number = random_object(1, 100); // The function pick a number between 1 and 100
     int       round          = 1;
 
-    std::cout << "\nWelcome to Guess the number !\nPick a number between 1 and 100\n";
+    // First round
+    std::cout << "\nWelcome to Guess the Number !\nRules are simple ! I pick a number between 1 and 100 and you have to guess it !\nPretty simple isn 't it ? Let' s start !\nPick a number between 1 and 100\n";
     int picked_number;
     std::cin >> picked_number;
 
+    // Game Loop
     while (picked_number != unknown_number) {
         if (picked_number > unknown_number) {
-            std::cout << "Smaller\n\n/----------------------------------------------------/\n";
+            std::cout << "It's smaller\n\n/----------------------------------------------------/\n";
         }
         else {
-            std::cout << "Greater\n\n/----------------------------------------------------/\n";
+            std::cout << "It's greater\n\n/----------------------------------------------------/\n";
         }
-        std::cout << "\nTry again\n";
+        std::cout << "\nLet's try again ! Pick a number between 1 and 100\n";
         std::cin >> picked_number;
         round++;
     }
-    std::cout << "\nCongrats, you won in " << round << " rounds !\n\n";
+    std::cout << "\nCongratulations human, you won in " << round << " rounds !\n\n";
 }
