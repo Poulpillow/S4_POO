ef7f5850c94b84117f3e2df41783341124a17d5f
Clémence <clemvoegele@gmail.com>
2022-06-06 16:00:23 +0200

    [hangman] Add comments + Fix game design

--- src/hangman.cpp
+++ src/hangman.cpp
@@ -7,10 +7,12 @@ std::string random_word()
 {
     const std::vector<std::string> word_list = {
         "imac",
-        "animaux",
-        "pays",
-        "bonsoir",
-        "cochon"};
+        "baguette",
+        "code",
+        "jules",
+        "talin",
+        "crepe",
+        "vacances"};
     const int         index = random_object(0, static_cast<int>(word_list.size()) - 1);
     const std::string word  = word_list[index];
     return word;
@@ -18,53 +20,50 @@ std::string random_word()
 
 void play_hangman()
 {
-    // Phrases de début (Bienvenue + Règles + Nombres de vies)
-    std::cout << "Welcome to Hangman !\nThe rules are simple : you have 10 lives to guess the secret word\nLet's get started !\n";
+    //-----------------------------------------------//
+    //------------ Beginning of the Game ------------//
+    //-----------------------------------------------//
 
-    //  Créer une liste de mots + Choisir un mot dans cette liste
+    std::cout << "\nWelcome to Hangman !\nRules are simple : you have 10 lives to guess the secret word !\nLet's get started !\n";
 
+    //  Choose a random word in the list of the function random_word() + keep the lengh of the word
     const std::string unknown_word       = random_word();
     const size_t      lengh_unknown_word = unknown_word.size();
 
-    // Créer le mot caché (avec le bon nombre de tirets)
-    // Créer un tableau de la longueur du mot inconnu
-    // Le remplir de tirets
-    // Faire en sorte que l'on peut modifier dynamiquement le mot inconnu + que l'on puisse accéder à chaque élément du tableau
-
+    // Creating the hidden word
     std::vector<char> hidden_word = {};
     for (size_t i = 0; i < lengh_unknown_word; i++) {
         hidden_word.push_back('_');
     }
 
-    // Boucle :
-    // - Afficher le mot caché
-    // - Afficher le nombre de vies
-    // - Demander une lettre
-    // - Récup de la lettre
-    // - Tester la lettre
-    // - Modifier le mot caché OU enlever de la vie
-    // - Continuer tant que le mot caché contient des tirets OU qu'il reste de la vie
-
+    // Set the variables
     int  number_lives        = 10;
     bool hidden_word_founded = false;
     char picked_letter       = ' ';
 
+    //-----------------------------------------------//
+    //-------------------- Loop ---------------------//
+    //-----------------------------------------------//
+
     while (number_lives != 0 && !hidden_word_founded) {
+        // Draw the text of the actual round
         std::string show_hidden_word(hidden_word.begin(), hidden_word.end());
-        std::cout << "Here is the current hidden word :\n"
-                  << show_hidden_word << "\nYou have " << number_lives << " lives\nChoose a letter\n";
+        std::cout << "\n/----------------------------------------------------/\n\nHere is the current hidden word : "
+                  << show_hidden_word << "\nYou have " << number_lives << " lives\n\nChoose a letter\n";
         std::cin >> picked_letter;
 
+        // Is there the letter if the word ?
         size_t position = unknown_word.find(picked_letter);
-
+        // Yes
         if (position != std::string::npos) {
+            // Change of the guessed letters
             for (size_t i = 0; i < lengh_unknown_word; i++) {
                 if (unknown_word.at(i) == picked_letter) {
                     hidden_word[i] = picked_letter;
                 }
             }
 
-            // A AMELIORER
+            // If the hidden is complete : stop the loop
             hidden_word_founded = true;
             for (size_t i = 0; i < lengh_unknown_word; i++) {
                 if (hidden_word[i] == '_') {
@@ -72,20 +71,28 @@ void play_hangman()
                 }
             }
         }
+        // No
         else {
             number_lives--;
         }
     }
 
-    // Message de fin (gagné ou perdu)
+    //-----------------------------------------------//
+    //--------------- End of the Game ---------------//
+    //-----------------------------------------------//
 
+    // You won
     if (hidden_word_founded) {
-        std::cout << "Congrats ! You found the word ! It was " << unknown_word << "\n\n";
+        std::cout << "\nCongrats ! You found the word ! It was " << unknown_word << "\n\n";
     }
+
+    // You lost
     else if (number_lives == 0) {
-        std::cout << "Oh no ! You lost all you lives ... Sorry bro\nThe word was " << unknown_word << "\n\n";
+        std::cout << "\nOh no ! You lost all you lives ... Sorry bro\nThe word was " << unknown_word << "\n\n";
     }
+
+    // Error
     else {
-        std::cout << "ERROR : THAT CAN NEVER HAPPEND !!!\n\n";
+        std::cout << "\nERROR : THAT CAN NEVER HAPPEND !!!\n\n";
     }
 }
